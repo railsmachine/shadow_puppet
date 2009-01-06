66dccc5809fb959ead265f381ce607f8d730b0e2
Jesse Newland <jnewland@gmail.com>
2009-01-06 14:00:21 -0500

    refactoring of the deploy process

--- lib/moonshine/manifest/rails.rb
+++ lib/moonshine/manifest/rails.rb
@@ -40,44 +40,69 @@ class Moonshine::Manifest::Rails < Moonshine::Manifest
         app_root = "/srv/rails/#{application}"
         repo_path = "/var/lib/moonshine/applications/#{applications}"
 
-        exec "#{application}-clone-repo",
-          :command  => "/usr/bin/git clone #{repo_path} #{app_root}",
-          :creates  => app_root,
-          :require  => reference(:exec, "#{application}-repo-perms"),
-          :notify   => reference(:exec, "#{application}-create-release-branch"),
-          :user     => "rails"
-
-        exec "#{application}-deploy-if-changes",
-          :cwd      => app_root,
+        exec "#{application}-begin"
           :command  => "/bin/true",
-          :unless   => "/usr/bin/test -d #{app_root} && /usr/bin/git checkout #{config[:branch]} && /usr/bin/git pull origin #{config[:branch]} 2> /dev/null | grep 'up-to-date' > /dev/null",
-          :require  => reference(:exec, "#{application}-repo-perms"),
-          :notify   => reference(:exec, "#{application}-create-release-branch"),
-          :user     => "rails"
+          :before   => reference(:exec, "#{application}-setup")
 
-        exec "#{application}-repo-perms",
-          :command      => "/bin/chgrp -R rails #{repo_path}",
-          :refreshonly  => true,
-          :require      => [
+        exec "#{application}-setup"
+          :command => "/bin/true",
+          :require => [
             reference(:user, "rails"),
             reference(:file, "/srv/rails"),
             reference(:exec, "#{application}-db"),
-            reference(:file, "#{application}-vhost")
+            reference(:file, "#{application}-vhost"),
+            reference(:package, "libapache2-mod-passenger"),
+            reference(:package, "rails")
+          ],
+          :before => [
+            reference(:exec, "#{application}-clone"),
+            reference(:exec, "#{application}-update")
           ]
 
-        exec "#{application}-create-release-branch",
-          :cwd          => app_root,
-          :command      => "/usr/bin/git checkout -b `date -u +%Y%m%d%H%M%N`",
-          :refreshonly  => true,
-          :user         => "rails"
+        exec "#{application}-update"
+          :command  => "/bin/true",
+          :onlyif   => "/usr/bin/test -d #{app_root}",
+          :require => [
+            reference(:exec, "#{application}-update-repo")
+          ],
+          :before   => reference(:exec, "#{application}-finalize-update")
+
+        exec "#{application}-clone"
+          :command  => "/bin/true",
+          :unless   => "/usr/bin/test -d #{app_root}",
+          :require => [
+            reference(:exec, "#{application}-clone-repo")
+          ],
+          :before   => reference(:exec, "#{application}-finalize-update")
+
+        exec "#{application}-finalize-update"
+          :command => "/bin/true",
+          :require => [
+            reference(:exec, "#{application}-migrate"),
+            reference(:exec, "#{application}-create-release-branch")
+          ],
+          :before => reference(:exec, "#{application}-restart")
+
+        exec "#{application}-restart"
+          :command => "/bin/true",
+          :require => [
+            reference(:exec, "#{application}-restart-passenger")
+          ],
+          :before => reference(:exec, "#{application}-finish")
+
+        exec "#{application}-finish"
+          :command => "/bin/true"
+
+        #setup
 
         #TODO parse database.yml if one exists. if not, create one.
 
         exec "#{application}-db",
-            :command  => "/usr/bin/mysqladmin create #{application}_production",
-            :unless   => "/usr/bin/mysqlcheck -s #{application}_production",
-            :require  => reference(:service, "mysql"),
-            :notify   => reference(:exec, "#{application}-db-user")
+            :command      => "/usr/bin/mysqladmin create #{application}_production",
+            :unless       => "/usr/bin/mysqlcheck -s #{application}_production",
+            :require      => reference(:service, "mysql"),
+            :notify       => reference(:exec, "#{application}-db-user"),
+            :refreshonly  => true
 
         exec "#{application}-db-user",
             :command      => "/usr/bin/mysql -e 'grant all privileges on #{application}_production.* to #{application}@localhost identified by \"password\"'",
@@ -86,15 +111,53 @@ class Moonshine::Manifest::Rails < Moonshine::Manifest
         #apache config
 
         file "#{application}-vhost",
-          :path     => "/etc/apache2/sites-available/#{application}",
-          :content  => ERB.new(File.read(File.join(File.dirname(__FILE__), '..', '..', 'templates', 'vhost.conf.erb'))).result(binding),
-          :require  => reference(:package, "apache2.2-common"),
-          :notify   => reference(:exec, "#{application}-enable-vhost")
+          :path         => "/etc/apache2/sites-available/#{application}",
+          :content      => ERB.new(File.read(File.join(File.dirname(__FILE__), '..', '..', 'templates', 'vhost.conf.erb'))).result(binding),
+          :require      => reference(:package, "apache2.2-common"),
+          :refreshonly  => true,
+          :notify       => reference(:exec, "#{application}-enable-vhost")
 
         exec "#{application}-enable-vhost",
             :command      => "/usr/sbin/a2ensite #{application}",
             :refreshonly  => true,
             :notify       => reference(:service, "apache2")
+
+        #clone
+
+        exec "#{application}-clone-repo",
+          :command      => "/usr/bin/git clone #{repo_path} #{app_root}",
+          :creates      => app_root,
+          :require      => reference(:exec, "#{application}-repo-perms"),
+          :refreshonly  => true,
+          :user         => "rails"
+
+        #update
+
+        exec "#{application}-update-repo",
+          :cwd          => app_root,
+          :command      => "/bin/true",
+          :unless       => "/usr/bin/git checkout #{config[:branch]} && /usr/bin/git pull origin #{config[:branch]} 2> /dev/null | grep 'up-to-date' > /dev/null",
+          :require      => reference(:exec, "#{application}-repo-perms"),
+          :refreshonly  => true,
+          :user         => "rails"
+
+        exec "#{application}-repo-perms",
+          :command      => "/bin/chgrp -R rails #{repo_path}",
+          :refreshonly  => true
+
+        #finalize-update
+
+        exec "#{application}-migrate",
+          :cwd          => app_root,
+          :command      => "RAILS_ENV=production rake:db:migrate",
+          :refreshonly  => true,
+          :user         => "rails"
+
+        exec "#{application}-create-release-branch",
+          :cwd          => app_root,
+          :command      => "/usr/bin/git checkout -b `date -u +%Y%m%d%H%M%N`",
+          :refreshonly  => true,
+          :user         => "rails"
 
         #run rake moonshine
 
@@ -111,10 +174,6 @@ class Moonshine::Manifest::Rails < Moonshine::Manifest
         exec "#{application}-restart-passenger",
             :command      => "/usr/bin/touch #{app_root}/tmp/restart.txt",
             :refreshonly  => true,
-            :require      => [
-              reference(:package, "libapache2-mod-passenger"),
-              reference(:package, "rails")
-            ],
             :user         => "rails"
 
       end
@@ -157,7 +216,8 @@ class Moonshine::Manifest::Rails < Moonshine::Manifest
           :enable          => true,
           :hasrestart      => true,
           :hasstatus       => true,
-          :require         => reference(:package, "apache2.2-common")
+          :require         => reference(:package, "apache2.2-common"),
+          :before          => reference(:file, "disable-default-site")
 
       file "disable-default-site",
           :path     => "/etc/apache2/sites-enabled/000-default",
