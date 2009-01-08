1dbd372ef9c20fbdf931154db0c32e184e605d2a
Jesse Newland <jnewland@gmail.com>
2009-01-08 10:20:26 -0500

    argh

--- lib/moonshine/cli.rb
+++ lib/moonshine/cli.rb
@@ -58,7 +58,7 @@ module Moonshine
       branch = console.ask("Deploy from branch:") { |q| q.default = "release" }
 
       user = console.ask("User: (this user will created if it doesn't already exist)") { |q| q.default = "rails" }
-      user = user(/\s*/,'')
+      user = user.gsub(/\s*/,'')
       setup_user(user)
 
       #save initial version of the config
