88f64f51941ee331943c17a623564f3cfc51bb89
Jesse Newland <jnewland@gmail.com>
2009-01-09 16:17:38 -0500

    fix ruby

--- lib/moonshine/modules/ruby.rb
+++ lib/moonshine/modules/ruby.rb
@@ -1,80 +1,87 @@
 module MoonshineRuby
   mattr_accessor :interpreter
 
-  def ruby(interpreter = :enterprise, uri = "")
+  def ruby(interpreter, uri = "")
     self.interpreter = interpreter
     begin
       tarball = uri.split("/").last
       stub = tarball.gsub(/\.tar\.gz/, '').gsub(/\.tgz/, '')
     rescue
     end
+    case interpreter
+    when :enterprise
+
     role "ruby" do
 
       file "/var/lib/moonshine/packages",
         :ensure => "directory"
 
-      case interpreter
-      when :enterprise
+      package "wget", :ensure => "installed"
 
-        package "wget", :ensure => "installed"
+      exec "download-enterprise",
+        :cwd      => "/var/lib/moonshine/packages",
+        :command  => "/usr/bin/wget #{uri}",
+        :creates  => "/var/lib/moonshine/packages/#{tarball}",
+        :before   => exec("untar-enterprise"),
+        :requires   => [
+           package("apache2-prefork-dev"),
+           package("libmysqlclient-dev"),
+           package("build-essential"),
+           package("zlib1g-dev"),
+           package("libssl-dev")
+         ]
 
-        exec "download-enterprise",
-          :cwd      => "/var/lib/moonshine/packages",
-          :command  => "/usr/bin/wget #{uri}",
-          :creates  => "/var/lib/moonshine/packages/#{tarball}",
-          :before   => exec("untar-enterprise"),
-          :requires   => [
-             package("apache2-prefork-dev"),
-             package("libmysqlclient-dev"),
-             package("build-essential"),
-             package("zlib1g-dev"),
-             package("libssl-dev")
-           ]
+      exec "untar-enterprise",
+        :cwd          => "/var/lib/moonshine/packages",
+        :command      => "/usr/bin/tar xzvf /var/lib/moonshine/packages/#{tarball}",
+        :creates      => "/var/lib/moonshine/packages/#{tarball}",
+        :refreshonly  => true,
+        :subscribe    => exec("download-enterprise"),
+        :before       => exec("install-enterprise")
 
-        exec "untar-enterprise",
-          :cwd          => "/var/lib/moonshine/packages",
-          :command      => "/usr/bin/tar xzvf /var/lib/moonshine/packages/#{tarball}",
-          :creates      => "/var/lib/moonshine/packages/#{tarball}",
-          :refreshonly  => true,
-          :subscribe    => exec("download-enterprise"),
-          :before       => exec("install-enterprise")
+      exec "install-enterprise",
+        :command      => "/usr/bin/yes | /var/lib/moonshine/packages/#{stub}/installer -a /opt/#{stub}",
+        :refreshonly  => true,
+        :subscribe    => exec("download-enterprise"),
+        :before       => exec("symlink-enterprise")
 
-        exec "install-enterprise",
-          :command      => "/usr/bin/yes | /var/lib/moonshine/packages/#{stub}/installer -a /opt/#{stub}",
-          :refreshonly  => true,
-          :subscribe    => exec("download-enterprise"),
-          :before       => exec("symlink-enterprise")
+      exec "symlink-enterprise",
+        :command      => "/bin/ln -fs /opt/#{stub} /opt/ree",
+        :refreshonly  => true,
+        :subscribe    => exec("install-enterprise"),
+        :before       => exec("symlink-rake")
 
-        exec "symlink-enterprise",
-          :command      => "/bin/ln -fs /opt/#{stub} /opt/ree",
-          :refreshonly  => true,
-          :subscribe    => exec("install-enterprise"),
-          :before       => exec("symlink-rake")
+      exec "symlink-rake",
+        :command      => "/bin/ln -fs /opt/ree/bin/rake /usr/bin/rake",
+        :refreshonly  => true,
+        :subscribe    => exec("symlink-enterprise"),
+        :before       => exec("symlink-gem")
 
-        exec "symlink-rake",
-          :command      => "/bin/ln -fs /opt/ree/bin/rake /usr/bin/rake",
-          :refreshonly  => true,
-          :subscribe    => exec("symlink-enterprise"),
-          :before       => exec("symlink-gem")
+      exec "symlink-gem",
+        :command      => "/bin/ln -fs /opt/ree/bin/gem /usr/bin/gem",
+        :refreshonly  => true,
+        :subscribe    => exec("symlink-rake"),
+        :before       => exec("install-passenger")
 
-        exec "symlink-gem",
-          :command      => "/bin/ln -fs /opt/ree/bin/gem /usr/bin/gem",
-          :refreshonly  => true,
-          :subscribe    => exec("symlink-rake"),
-          :before       => exec("install-passenger")
+      exec "install-passenger",
+        :command      => "/usr/bin/yes /opt/ree/bin/passenger-install-apache2-module",
+        :refreshonly  => true,
+        :subscribe    => exec("symlink-rake"),
+        :before       => exec("install-ruby")
 
-        exec "install-passenger",
-          :command      => "/usr/bin/yes /opt/ree/bin/passenger-install-apache2-module",
-          :refreshonly  => true,
-          :subscribe    => exec("symlink-rake"),
-          :before       => exec("install-ruby")
+      exec "install-ruby",
+        :refreshonly  => true,
+        :subscribe    => exec("install-passenger"),
+        :command      => "/bin/true"
 
-        exec "install-ruby",
-          :refreshonly  => true,
-          :subscribe    => exec("install-passenger"),
-          :command      => "/bin/true",
+    end
 
-      when :ubuntu
+    when :debian
+
+    role "ruby" do
+
+      file "/var/lib/moonshine/packages",
+        :ensure => "directory"
 
         package "rubygems", :ensure => "installed"
         package "rake", :ensure => "installed"
