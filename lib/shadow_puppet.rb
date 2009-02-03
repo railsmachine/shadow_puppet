5a5d1c6c2c76b9bb70d5ae3a19e7478bbca7d776
Jesse Newland <jnewland@gmail.com>
2009-02-03 14:51:30 -0500

    ensure ShadowPuppet can run when required

--- lib/shadow_puppet.rb
+++ lib/shadow_puppet.rb
@@ -1,1 +1,24 @@
 require File.join(File.dirname(__FILE__) + '/shadow_puppet', 'manifest.rb')
+
+class ShadowPuppet::Manifest::Setup < ShadowPuppet::Manifest
+  recipe :setup_directories
+
+  def setup_directories()
+    if Process.uid == 0
+      file "/var/puppet",
+        :ensure => "directory",
+        :backup => false
+    else
+      file ENV["HOME"] + "/.puppet",
+        :ensure => "directory",
+        :backup => false
+      file ENV["HOME"] + "/.puppet/var",
+        :ensure   => "directory",
+        :backup   => false,
+        :require  => file(ENV["HOME"] + "/.puppet")
+    end
+  end
+end
+
+setup = ShadowPuppet::Manifest::Setup.new
+setup.execute
