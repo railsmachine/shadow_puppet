9d2105bd9de2d952c4686e2ebaadd0be9d60a196
Joshua Nichols <josh@technicalpickles.com>
2010-06-28 23:54:20 -0400

    Switch to isolate for isolating versions.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,31 +1,18 @@
 require 'rubygems'
 require 'ruby-debug'
-require 'bundler'
+require 'isolate'
 
-Bundler.module_eval do
-  class << self
-    def root
-      Pathname.new(__FILE__).dirname.dirname
-    end
+sandbox = Isolate::Sandbox.new do
+  gem 'puppet', '= 0.24.8'
+  gem 'facter', '>= 1.5.4'
+  gem 'highline', '>= 1.5.0'
+  gem 'builder', '>= 2.1.2'
+  gem 'activesupport', "= #{ENV['ACTIVESUPPORT_VERSION'] || '2.3.8'}"
 
-    def definition
-      builder = Bundler::Dsl.new
-      builder.instance_eval do
-        gem 'puppet', '= 0.24.8'
-        gem 'facter', '>= 1.5.4'
-        gem 'highline', '>= 1.5.0'
-        gem 'builder', '>= 2.1.2'
-        gem 'activesupport', "= #{ENV['ACTIVESUPPORT_VERSION'] || '2.3.8'}"
-        group :test do
-          gem 'rspec', '~> 1.3.0'
-        end
-      end
-      builder.to_definition
-    end
-  end
+  gem 'rspec', '~> 1.3.0'
 end
 
-Bundler.runtime.setup(:default, :test)
+sandbox.activate
 
 require 'spec'
 require 'spec/autorun'
@@ -37,3 +24,5 @@ require 'shadow_puppet/test'
 Dir.glob(File.join(File.dirname(__FILE__), 'fixtures', '*.rb')).each do |manifest|
   require manifest
 end
+
+puts "Testing against activesupport-#{ActiveSupport::VERSION::STRING}"
