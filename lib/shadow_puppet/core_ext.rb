aa377166841b38d4210389da84f9fa49fa63bc0c
Jesse Newland <jnewland@gmail.com>
2010-08-18 13:48:01 -0400

    consolidate our activesupport hax

--- lib/shadow_puppet/core_ext.rb
+++ lib/shadow_puppet/core_ext.rb
@@ -1,11 +1,23 @@
 require 'active_support/version'
-
 require 'active_support/core_ext/class/attribute_accessors'
 require 'active_support/core_ext/array'
 require 'active_support/inflector'
 require 'active_support/core_ext/class/inheritable_attributes'
 
+# zomg epic hax
 if ActiveSupport::VERSION::MAJOR < 3
+  if ActiveSupport::VERSION::TINY > 5
+    # hack around absurd number of deprecation warnings from puppet
+    # using `metaclass`
+    require 'active_support/core_ext/kernel/reporting'
+    require 'active_support/core_ext/module/attribute_accessors'
+    require 'active_support/deprecation'
+    ActiveSupport::Deprecation.silenced = true
+  end
+  require 'active_support/core_ext/string/inflections'
+  unless String.included_modules.include?(ActiveSupport::CoreExtensions::String::Inflections)
+    String.send :include, ActiveSupport::CoreExtensions::String::Inflections
+  end
   require 'active_support/core_ext/duplicable'
 else
   require 'active_support/core_ext/object/duplicable'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'isolate/scenarios/now'
 gem 'rspec'
 require 'spec'
+require File.join(File.dirname(__FILE__), '..', 'lib', 'shadow_puppet', 'core_ext.rb')
 require File.join(File.dirname(__FILE__), '..', 'lib', 'shadow_puppet.rb')
 require File.join(File.dirname(__FILE__), '..', 'lib', 'shadow_puppet', 'test.rb')
 Dir.glob(File.join(File.dirname(__FILE__), 'fixtures', '*.rb')).each do |manifest|
